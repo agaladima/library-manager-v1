9ff741c6a009ac9eeb5b060fa19acebf1d20d2bf
Arum <arum.galadima@gmail.com>
2018-04-23 23:25:08 -0400

    Initial commit

---
+++ models/loan.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Loan = sequelize.define('Loan', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    book_id: DataTypes.INTEGER,
+    patron_id: DataTypes.INTEGER,
+    loaned_on: DataTypes.DATE,
+    return_by: DataTypes.DATE,
+    returned_on: DataTypes.DATE
+  }, {});
+  Loan.associate = function(models) {
+    // associations can be defined here
+  };
+  return Loan;
+};
